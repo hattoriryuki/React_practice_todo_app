7299bc10022b1ce41b581a2f0708e59e9adcd2db
hattori_ryuki <94377268+hattoriryuki@users.noreply.github.com>
2022-08-30 01:22:23 +0900

    Fix: Reactを意識したモックに修正

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState(["TODO１", "TODO2"]);
+  const [completeTodos, setcompleteTodos] = useState(["TODO3"]);
+
   return (
     <>
       <div className="input-area">
@@ -11,25 +14,28 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          <div className="list-row">
-            <li>TODO１</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
-          <div className="list-row">
-            <li>TODO２</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
+          {incompleteTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
-          <div className="list-row">
-            <li>TODO3</li>
-            <button>戻す</button>
-          </div>
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
     </>
